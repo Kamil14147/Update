2798307e5b146ca7ca6327ddedcc43e3b63d8071
Kamil14147 <145575644+Kamil14147@users.noreply.github.com>
2026-06-04 21:15:45 +0200

    Update firmware version to 1.0.1

--- esp32/KejmilOLED/KejmilOLED.ino
+++ esp32/KejmilOLED/KejmilOLED.ino
@@ -26,7 +26,7 @@ static const bool BUTTON_ENABLED = true;
 
 static const char *DEVICE_ID = "kejmil-oled-esp32";
 static const char *BLE_DEVICE_NAME = "Kejmil OLED";
-static const char *FW_VERSION = "1.0.0";
+static const char *FW_VERSION = "1.0.1";
 static const char *UART_SERVICE_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
 static const char *UART_RX_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
 static const char *UART_TX_UUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
